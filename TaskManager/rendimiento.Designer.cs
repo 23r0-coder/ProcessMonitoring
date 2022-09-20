fa56cf9e98ea7fb607b1436bf2025d1c58292fda
23_ϱ <84030511+23r0-coder@users.noreply.github.com>
2022-09-19 22:06:37 -0600

    Add files via upload

---
+++ TaskManager/rendimiento.Designer.cs
@@ -0,0 +1,54 @@
+﻿namespace TaskManager
+{
+    partial class panelRendimiento
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // panelRendimiento
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 397);
+            this.DisplayHeader = false;
+            this.Movable = false;
+            this.Name = "panelRendimiento";
+            this.Padding = new System.Windows.Forms.Padding(20, 30, 20, 20);
+            this.Resizable = false;
+            this.ShadowType = MetroFramework.Forms.MetroFormShadowType.None;
+            this.Style = MetroFramework.MetroColorStyle.White;
+            this.Text = "rendimiento";
+            this.Theme = MetroFramework.MetroThemeStyle.Default;
+            this.Load += new System.EventHandler(this.panelRendimiento_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
